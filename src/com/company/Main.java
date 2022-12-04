6684151f6ddad4a870827c3ad1c399cf292990bc
Tykher <km.maliszewski@gmail.com>
2022-12-04 23:01:15 +0100

    Created chain

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import com.company.HandlerExtensions.AdditionHandler;
+import com.company.HandlerExtensions.DivisionHandler;
+import com.company.HandlerExtensions.MultiplicationHandler;
+import com.company.HandlerExtensions.SubtractionHandler;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Handler chain = new AdditionHandler(new SubtractionHandler(new MultiplicationHandler(new DivisionHandler(null))));
+
     }
 }
